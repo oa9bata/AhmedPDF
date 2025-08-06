1acc120b8f42c602eaa3598a5fbd3d3533577de0
oa9bata <batawelofficial@gmail.com>
2025-08-06 04:50:35 +0300

    9th commit

---
+++ config.js
@@ -0,0 +1,5 @@
+window.ENV = {
+    SUPABASE_URL: 'https://jrlbailragggxaiteclv.supabase.co',
+    API_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpybGJhaWxyYWdnZ3hhaXRlY2x2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQyOTg0NDEsImV4cCI6MjA2OTg3NDQ0MX0.2_iWzgbeXSYMfIMflnOblF9k-L8JQRwBhSwGhAGPOCI',
+    ADMIN_EMAIL: 'batawelofficial@gmail.com'
+}; 
